568da001c90fa75a4f4bbb18eff7bfbe78c99c3c
David ROUMANET <golfy@free.fr>
2021-02-13 16:27:46 +0100

    Lecture Velbus fonctionne (commit correct)

--- index.js
+++ index.js
@@ -1,9 +1,30 @@
-var http = require('http');
+let http = require('http');
+let io = require("socket.io");
+let net = require("net");
 
-var server = http.createServer(function(request, response){
+let host = "192.168.168.248";
+let port = 8445;
+let client = new net.Socket();
+let connexion = () => {
+    console.log("connected to velbus server...");
+}
+client.connect(port, host, connexion);
+
+client.on('data', (data) => {
+    console.log("DATA: "+data);
+});
+client.on('close', ()  => {
+    console.log("CLOSING CONNEXION");
+});
+
+
+let server = http.createServer(function(request, response){
     response.writeHead(200, {'Content-Type': 'text/html'});
     response.write('hello world');
     response.end();
 });
 
+
+// Le serveur écoute les requêtes http sur le port 8001 mais aussi les requêtes "socket"
 server.listen(8001);
+io.listen(server);
