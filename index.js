33499d84d938a4159ac1e96c41cf0822b2c23660
David ROUMANET <golfy@free.fr>
2021-07-16 18:14:22 +0200

    remove semi-comma

--- index.js
+++ index.js
@@ -1,52 +1,66 @@
 'use strict';
 // Partie serveur Node.JS classique
-let app = require('express')();
-let express = require('express');
+let path = require('path')
+let express = require('express')
+let app = require('express')()
+app.set('view engine', 'ejs')
+app.set('views',path.join(__dirname+'/views/'))
+app.use(express.static('views'))
+
+// TODO - Timer part (see https://crontab.guru)
+let schedule = require('node-schedule');
+let eventHour = schedule.scheduleJob('* */1 * * *', () => {
+    // call energy counter
+})
+let event5min = schedule.scheduleJob('*/5 * * * *', () => {
+    // call temperature status
+})
+
 let http = require('http').createServer(app);
-let bodyparser = require('body-parser');
 let io = require("socket.io")(http);        // create websocket with existing port HTTP
 
-let Routeur = require('./routes/routes');
-app.set('view engine', 'ejs');
-app.use(express.static('views'));
-let portWeb = 8002;
+let bodyparser = require('body-parser')    // middleware
+let Routeur = require('./routes/routes')
+app.get('/', Routeur)
+// FIXME - problem on /installation (view not accessible)
 
 // Partie Velbus
-let VMBserver = require('./VMBServer.ini');
-let traitement = require('./controllers/traitement');
-let velbusServer = require('./controllers/velbuslib');
-velbusServer.VelbusStart(VMBserver.host, VMBserver.port);
+let VMBserver = require('./VMBServer.ini')
+let traitement = require('./controllers/traitement')
+let velbusServer = require('./controllers/velbuslib')
+velbusServer.VelbusStart(VMBserver.host, VMBserver.port)
+let moduleList = []
+
 
 // établissement de la connexion
 // let listenClients = io.listen(http);
 io.on('connection', (socket) =>{
-    console.log(`Connecté au client ${socket.id}`);
+    console.log(`Connecté au client ${socket.id}`)
     socket.on('relay', (msg) => {
-        console.log("Action sur le relay : ", msg, velbusServer.toHexa(velbusServer.relaySet(0x02, 1, 1)));
-        if (msg == "ON") velbusServer.VMBWrite(velbusServer.relaySet(0x2E, 1, 1));
-        if (msg == "OFF") velbusServer.VMBWrite(velbusServer.relaySet(0x2E, 1, 0));
+        console.log("► ",msg)
+        if (msg.status == "ON") velbusServer.VMBWrite(velbusServer.relaySet(msg.address, msg.part, 1))
+        if (msg.status == "OFF") velbusServer.VMBWrite(velbusServer.relaySet(msg.address, msg.part, 0))
+        console.log("Action sur le relay : ", msg, "address:", msg.address);
     });
     socket.on('blind', (msg) => {
-        console.log("Action sur le volet : ", msg, velbusServer.toHexa(velbusServer.blindMove(0x2C, 1, -1, 1)));
-        if (msg == "DOWN") velbusServer.VMBWrite(velbusServer.blindMove(0x2C, 1, -1, 1));
-        if (msg == "UP") velbusServer.VMBWrite(velbusServer.blindMove(0x2C, 1, 1, 1));
-        if (msg == "STOP") velbusServer.VMBWrite(velbusServer.blindStop(0x2C, 1));
-    });
+        if (msg.status == "DOWN") velbusServer.VMBWrite(velbusServer.blindMove(msg.address, msg.part, -1, 10))
+        if (msg.status == "UP") velbusServer.VMBWrite(velbusServer.blindMove(msg.address, msg.part, 1, 10))
+        if (msg.status == "STOP") velbusServer.VMBWrite(velbusServer.blindStop(msg.address, msg.part))
+        console.log("Action sur le volet : ", msg)
+    })
  });
  velbusServer.VMBEmitter.on("msg", (dataSend) => {
-     console.log("Envoi ⏩ ",dataSend)
+     console.log("Envoi ⏩ ",dataSend.RAW)
      io.emit("msg", dataSend)
  });
 
-// lancement serveur NodeJS
+// lancement serveur NodeJS sur le port 8002
+let portWeb = 8002;
 http.listen(portWeb, () => {
-    console.log("Web app listening on port ", portWeb);
+    console.log("Web app listening on port ", portWeb)
 });
-
-
-app.get('/', Routeur);
 
 
 // Le serveur écoute les requêtes http sur le port 8001 mais aussi les requêtes "socket"
 // server.listen(8001);
-io.listen(http);
+io.listen(http)
