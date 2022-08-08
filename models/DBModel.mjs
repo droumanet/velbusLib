0ccaf23a27abb7f90cbe06ebbadf45360d0355a1
prRoumanet <golfy@free.fr>
2022-08-08 23:18:16 +0200

    Reorder Velbus_class to Models
    Preparing code for Models and SQL part. Create a setPowerDay() function too.

--- models/DBModel.mjs
+++ models/DBModel.mjs
@@ -1,20 +1,28 @@
 import * as db from '../Database.mjs'
-    function getPower(callback){
-        let sql='SELECT * FROM pwrDay';
-        db.query(sql, function (err, data, fields) {
-            if (err) throw err;
-            return callback(data);
-        });
-    }
 
-    /*
-    addFlower:function(flowerDetails,callback){
-        var sql = 'INSERT INTO flori SET ?';
-        db.query(sql, flowerDetails,function (err, data) {
-            if (err) throw err;
-            return callback(data);
-        });
-    },
-    */
+function getPower(callback){
+    let sql='SELECT * FROM pwrDay';
+    db.query(sql, function (err, data, fields) {
+        if (err) throw err;
+        return callback(data);
+    });
+}
+
+function setPowerDay(callback){
+    let sql='SELECT * FROM pwrDay';
+    db.query(sql, function (err, data, fields) {
+        if (err) throw err;
+        return callback(data);
+    });
+}
+/*
+addFlower:function(flowerDetails,callback){
+    var sql = 'INSERT INTO flori SET ?';
+    db.query(sql, flowerDetails,function (err, data) {
+        if (err) throw err;
+        return callback(data);
+    });
+},
+*/
 
 export {getPower}
